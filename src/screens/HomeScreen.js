304fd7b5102899ebb1b165211d81f529a24b200d
iOS Developer <pratibha@pratibhas-MacBook-Pro.local>
2021-02-04 17:32:07 +0530

    changes in homescreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,6 +3,9 @@ import {
   View,
   Text,
   ImageBackground,
+  StyleSheet,
+  TouchableHighlight,
+  Modal,
   Dimensions,
   TouchableOpacity,
 } from 'react-native';
@@ -10,6 +13,7 @@ import {Icon} from 'react-native-elements';
 
 const {height, width} = Dimensions.get('window');
 export function HomeScreen({navigation}) {
+  const [modalVisible, setModalVisible] = React.useState(false);
   return (
     <ImageBackground
       source={require('../assets/holiday1.png')}
@@ -92,10 +96,15 @@ export function HomeScreen({navigation}) {
           height: height * 0.4,
           alignItems: 'flex-end',
           flexDirection: 'row',
-          justifyContent: 'space-between',
+          justifyContent: 'center',
         }}>
+          {/**
+           * 
          <View style={{margin: 10}}>
           <TouchableOpacity
+            onPress={() => {
+              setModalVisible(true);
+            }}
             style={{
               backgroundColor: '#ffe',
               padding: height * 0.02,
@@ -106,6 +115,7 @@ export function HomeScreen({navigation}) {
             <Text style={{fontSize: height * 0.02}}>Rate App</Text>
           </TouchableOpacity>
         </View> 
+           */}
         <View style={{margin: 10}}>
           <TouchableOpacity
             onPress={() => navigation.navigate('AboutUs')}
@@ -120,6 +130,73 @@ export function HomeScreen({navigation}) {
           </TouchableOpacity>
         </View>
       </View>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert("Modal has been closed.");
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
+              onPress={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
+
     </ImageBackground>
   );
 }
+
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+});
+
